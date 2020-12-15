5b33f9d9c5ef717ab69ae21f246088f12690cec6
Edith <edithebea@gmail.com>
2020-12-14 19:57:02 -0600

    entidades regisro de merma

---
+++ Clases/Entidades/MermaDetalle.vb
@@ -0,0 +1,139 @@
+﻿Public Class MermaDetalle
+    Private Const Tabla As String = "mermadetalle"
+    Private idmermadetalle As Integer
+    Private cantidadMerma As Integer
+    Private idreg_m As Integer
+    Private id_producti As Integer
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(idmermadetalle As Integer,
+                     cantidadMerma As Integer,
+                     idreg_m As Integer,
+                     id_producti As Integer)
+
+        Me.idmermadetalle = idmermadetalle
+        Me.cantidadMerma = cantidadMerma
+        Me.idreg_m = idreg_m
+        Me.id_producti = id_producti
+    End Sub
+    Public Sub SetIdMermaDetalle(idmermadetalle As Integer)
+        Me.idmermadetalle = idmermadetalle
+    End Sub
+    Public Sub SetCantidadMerma(cantidadMerma As Integer)
+        Me.cantidadMerma = cantidadMerma
+    End Sub
+    Public Sub SetIdRegistroMerma(idreg_m As Integer)
+        Me.idreg_m = idreg_m
+    End Sub
+    Public Sub SetIdProducto(id_producti As Integer)
+        Me.id_producti = id_producti
+    End Sub
+    Public Function GetIdMermaDetalle() As Integer
+        Return Me.idmermadetalle
+    End Function
+    Public Function GetCantidadMerma() As Integer
+        Return Me.cantidadMerma
+    End Function
+    Public Function GetIdRegistrarMerma() As Integer
+        Return Me.idreg_m
+    End Function
+    Public Function GetIdProducto() As Integer
+        Return Me.id_producti
+    End Function
+
+    Public Function AgregarMermaDetalle() As Boolean
+        Dim database As BaseDatos = New BaseDatos()
+        Dim columnas As String() = {"idmermadetalle", "cantidadMerma", "idreg_m", "id_producti"}
+        Dim valores As String() = {"'" & Me.idmermadetalle & "'", "'" & Me.cantidadMerma & "'", "'" & Me.idreg_m & "'",
+                                   "'" & Me.id_producti & "'"}
+        Dim result = database.Insertar(Tabla, columnas, valores)
+        Return result
+    End Function
+    Public Function ActualizarMermaDetalle() As Boolean
+        Dim database As BaseDatos = New BaseDatos()
+        Dim columnas As String() = {"idmermadetalle", "cantidadMerma", "idreg_m", "id_producti"}
+        Dim valores As String() = {"'" & Me.idmermadetalle & "'", "'" & Me.cantidadMerma & "'", "'" & Me.idreg_m & "'",
+                                   "'" & Me.id_producti & "'"}
+        Dim condiciones As String() = {"idmermadetalle=" & "'" & Me.idmermadetalle & "'"}
+        Dim result = database.Actualizar(Tabla, columnas, valores, condiciones)
+        Return result
+    End Function
+
+    Public Function EliminarMermaDetalle() As Boolean
+        Dim database As BaseDatos = New BaseDatos()
+        Dim condiciones As String() = {"idmermadetalle=" & "'" & Me.idmermadetalle & "'"}
+        Dim result = database.Eliminar(Tabla, condiciones)
+        Return result
+    End Function
+    Public Function BuscarMermaDetalleById(idusuario As Integer) As Boolean
+        Dim database As BaseDatos = New BaseDatos()
+        Dim columnas As String() = {"'" & Me.idmermadetalle & "'", "'" & Me.cantidadMerma & "'", "'" & Me.idreg_m & "'",
+                                   "'" & Me.id_producti & "'"}
+        Dim condiciones As String() = {"idmermadetalle=" & "'" & Me.idmermadetalle & "'"}
+        Dim result As DataTable
+
+        result = database.Buscar({Tabla}, columnas, condiciones)
+
+        If result.Rows.Count = 1 Then
+            If Not IsDBNull(result.Rows(0)("idmermadetalle")) And
+               Not IsDBNull(result.Rows(0)("cantidadMerma")) And
+               Not IsDBNull(result.Rows(0)("idreg_m")) And
+               Not IsDBNull(result.Rows(0)("id_producti")) Then
+                SetIdMermaDetalle(CInt(result.Rows(0)("idmermadetalle")))
+                SetCantidadMerma(CInt(result.Rows(0)("cantidadMerma")))
+                SetIdRegistroMerma(CInt(result.Rows(0)("idreg_m")))
+                SetIdProducto(CInt(result.Rows(0)("id_producti")))
+                Return True
+            Else
+                Throw New Exception("Error: Columna con valores vacios.")
+            End If
+        Else
+            Return False
+        End If
+    End Function
+    Public Function BuscarMermaDetalle() As DataTable
+        Dim database As BaseDatos = New BaseDatos()
+        Dim columnas As String() = {"idmermadetale", "cantidadMerma", "idreg_m", "id_producti"}
+
+        Return database.Buscar({Tabla}, columnas, {})
+    End Function
+    Public Function BuscarMermaDetalleDByConditions(columnasExtra As String(), joins As String(), condiciones As String()) As DataTable
+        Dim database As BaseDatos = New BaseDatos()
+        Dim columnas As String() = {Tabla & ".idmermadetalle", Tabla & ".cantidadMerma", Tabla & ".idreg_m",
+                                    Tabla & ".idproducti"}
+
+        Return database.Buscar({Tabla}, columnasExtra.Union(columnas).ToArray, joins, condiciones)
+    End Function
+    Public Function BuscarUltimoId() As Integer
+        Dim database As BaseDatos = New BaseDatos()
+        Dim columnas As String() = {"Max(idmermadetalle) AS idmermadetalle"}
+
+        Dim result As DataTable
+
+        result = database.Buscar({Tabla}, columnas, {})
+        If result.Rows.Count = 1 And Not IsDBNull(result.Rows(0)("idmermadetalle")) Then
+            Return CInt(result.Rows(0)("idmermadetalle"))
+        Else
+            Return 0
+        End If
+    End Function
+    'Public Sub PoblarComboProducto(cbPrducto As ComboBox)
+    '   cbPrducto.DisplayMember = "Value"
+    '  cbPrducto.ValueMember = "Key"
+    'Dim producto As DataTable = BuscarProducto()
+    '   producto.DefaultView.Sort = "idProductos ASC"
+    '  producto = producto.DefaultView.ToTable()
+
+    'If producto.Rows.Count > 0 Then
+    'Dim tipoUDictionary As New Dictionary(Of Integer, String)
+    'For index = 0 To producto.Rows.Count - 1
+    '           tipoUDictionary.Add(producto.Rows(index)("idProductos"), producto.Rows(index)("nombreProducto"))
+    'Next
+    '       cbPrducto.DataSource = New BindingSource(tipoUDictionary, Nothing)
+    'Else
+    '       cbPrducto.DataSource = Nothing
+    'End If
+    'End Sub
+End Class
---
+++ Clases/Entidades/RegMerma.vb
@@ -0,0 +1,136 @@
+﻿Public Class RegMerma
+    Private Const Tabla As String = "regmerma"
+    Private idregMerma As Integer
+    Private fechaReg As Date
+    Private descripcion As String
+    Private iduseer As Integer
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(idregMerma As Integer,
+                     fechaReg As Date,
+                     descripcion As String,
+                     iduseer As Integer)
+
+        Me.idregMerma = idregMerma
+        Me.fechaReg = fechaReg
+        Me.descripcion = descripcion
+        Me.iduseer = iduseer
+    End Sub
+    Public Sub SetIdRegistroMerma(idregMerma As Integer)
+        Me.idregMerma = idregMerma
+    End Sub
+    Public Sub SetFechaRegistro(fechaReg As Date)
+        Me.fechaReg = fechaReg
+    End Sub
+    Public Sub SetDescripcion(descripcion As String)
+        Me.descripcion = descripcion
+    End Sub
+    Public Sub SetIdUser(iduseer As Integer)
+        Me.iduseer = iduseer
+    End Sub
+
+    Public Function GetIdRegistroMerma() As Integer
+        Return Me.idregMerma
+    End Function
+    Public Function GetFechaRegistro() As Date
+        Return Me.fechaReg
+    End Function
+    Public Function GetDescripcion() As String
+        Return Me.descripcion
+    End Function
+    Public Function GetIdUser() As String
+        Return Me.iduseer
+    End Function
+
+    Public Function AgregarRegistroMerma() As Boolean
+        Dim database As BaseDatos = New BaseDatos()
+        Dim columnas As String() = {"idregMerma", "fechaReg", "descripcion", "iduseer"}
+        Dim valores As String() = {"'" & Me.idregMerma & "'", "'" & Me.fechaReg.ToString("yyyy-MM-dd") & "'", "'" & Me.descripcion & "'", "'" & Me.iduseer & "'"}
+        Dim result = database.Insertar(Tabla, columnas, valores)
+        Return result
+    End Function
+    Public Function ActualizarRegistroMerma() As Boolean
+        Dim database As BaseDatos = New BaseDatos()
+        Dim columnas As String() = {"idregMerma", "fechaReg", "descripcion", "iduseer"}
+        Dim valores As String() = {"'" & Me.idregMerma & "'", "'" & Me.fechaReg.ToString("yyyy-MM-dd") & "'", "'" & Me.descripcion & "'", "'" & Me.iduseer & "'"}
+        Dim condiciones As String() = {"idregMerma=" & "'" & Me.idregMerma & "'"}
+        Dim result = database.Actualizar(Tabla, columnas, valores, condiciones)
+        Return result
+    End Function
+
+    Public Function EliminarRegistroMerma() As Boolean
+        Dim database As BaseDatos = New BaseDatos()
+        Dim condiciones As String() = {"idregMerma=" & "'" & Me.idregMerma & "'"}
+        Dim result = database.Eliminar(Tabla, condiciones)
+        Return result
+    End Function
+    Public Function BuscarRegistroMermaById(idusuario As Integer) As Boolean
+        Dim database As BaseDatos = New BaseDatos()
+        Dim columnas As String() = {"idregMerma", "fechaReg", "descripcion", "iduseer"}
+        Dim condiciones As String() = {"idregMerma='" & idregMerma & "'"}
+        Dim result As DataTable
+
+        result = database.Buscar({Tabla}, columnas, condiciones)
+
+        If result.Rows.Count = 1 Then
+            If Not IsDBNull(result.Rows(0)("idregMerma")) And
+               Not IsDBNull(result.Rows(0)("fechaReg")) And
+               Not IsDBNull(result.Rows(0)("descripcion")) And
+               Not IsDBNull(result.Rows(0)("iduseer")) Then
+                SetIdRegistroMerma(CInt(result.Rows(0)("idregMerma")))
+                SetFechaRegistro(CStr(result.Rows(0)("fechaReg")))
+                SetDescripcion(CStr(result.Rows(0)("descripcion")))
+                SetIdUser(CInt(result.Rows(0)("iduseer")))
+                Return True
+            Else
+                Throw New Exception("Error: Columna con valores vacios.")
+            End If
+        Else
+            Return False
+        End If
+    End Function
+    Public Function BuscarRegistroMerma() As DataTable
+        Dim database As BaseDatos = New BaseDatos()
+        Dim columnas As String() = {"idregMerma", "fechaREG", "descripcion", "iduseer"}
+
+        Return database.Buscar({Tabla}, columnas, {})
+    End Function
+    Public Function BuscarRegistroMermaByConditions(columnasExtra As String(), joins As String(), condiciones As String()) As DataTable
+        Dim database As BaseDatos = New BaseDatos()
+        Dim columnas As String() = {Tabla & ".idregMerma", Tabla & ".fechaReg", Tabla & ".descripcion", Tabla & ".iduseer"}
+        Return database.Buscar({Tabla}, columnasExtra.Union(columnas).ToArray, joins, condiciones)
+    End Function
+    Public Function BuscarUltimoId() As Integer
+        Dim database As BaseDatos = New BaseDatos()
+        Dim columnas As String() = {"Max(idregMerma) AS idregMerma"}
+
+        Dim result As DataTable
+
+        result = database.Buscar({Tabla}, columnas, {})
+        If result.Rows.Count = 1 And Not IsDBNull(result.Rows(0)("idregMerma")) Then
+            Return CInt(result.Rows(0)("idregMerma"))
+        Else
+            Return 0
+        End If
+    End Function
+
+    Public Sub PoblarComboDescripcion(cbDescripcion As ComboBox)
+        cbDescripcion.DisplayMember = "Value"
+        cbDescripcion.ValueMember = "Key"
+        Dim rMerma As DataTable = BuscarRegistroMerma()
+        rMerma.DefaultView.Sort = "idregMerma ASC"
+        rMerma = rMerma.DefaultView.ToTable()
+
+        If rMerma.Rows.Count > 0 Then
+            Dim tipoUDictionary As New Dictionary(Of Integer, String)
+            For index = 0 To rMerma.Rows.Count - 1
+                tipoUDictionary.Add(rMerma.Rows(index)("idregMerma"), rMerma.Rows(index)("descripcion"))
+            Next
+            cbDescripcion.DataSource = New BindingSource(tipoUDictionary, Nothing)
+        Else
+            cbDescripcion.DataSource = Nothing
+        End If
+    End Sub
+End Class
